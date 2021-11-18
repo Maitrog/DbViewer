2fd82523ffa6d90ed6ab6950a985b10d07abab8b
Maitrog <Mihayla2001@gmail.com>
2021-11-18 21:42:21 +0300

    Add updating data page

---
+++ DbViewer/View/UpdateDataPageView.xaml.cs
@@ -0,0 +1,99 @@
+﻿using DbViewer.Model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DbViewer.View
+{
+    /// <summary>
+    /// Логика взаимодействия для UpdateDataPageView.xaml
+    /// </summary>
+    public partial class UpdateDataPageView : UserControl
+    {
+        public UpdateDataPageView()
+        {
+            InitializeComponent();
+            Loaded += UpdateDataPageView_Loaded;
+        }
+
+        private void UpdateDataPageView_Loaded(object sender, RoutedEventArgs e)
+        {
+            tables.ItemsSource = Db.GetTable();
+            tables.SelectionChanged += Tables_SelectionChanged;
+        }
+
+        private void Tables_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateTableData();
+        }
+
+        private void UpdateTableData()
+        {
+            dataGrid.Columns.Clear();
+            List<KeyValuePair<string, Type>> columns = Db.GetColumn(tables.SelectedItem.ToString());
+            DataTable dt = new DataTable();
+
+            DataColumn dataColumn;
+            foreach (var column in columns)
+            {
+                switch (column.Value.Name)
+                {
+                    case "Boolean":
+                        dataColumn = new DataColumn();
+                        dataColumn.DataType = Type.GetType("System.Boolean");
+                        dataColumn.ColumnName = column.Key;
+                        dt.Columns.Add(dataColumn);
+                        break;
+                    case "Int32":
+                        dataColumn = new DataColumn();
+                        dataColumn.DataType = Type.GetType("System.Int32");
+                        dataColumn.ColumnName = column.Key;
+                        dt.Columns.Add(dataColumn);
+                        break;
+                    default:
+                        dataColumn = new DataColumn();
+                        dataColumn.DataType = Type.GetType("System.String");
+                        dataColumn.ColumnName = column.Key;
+                        dt.Columns.Add(dataColumn);
+                        break;
+                }
+            }
+
+            var res = Db.GetAllFromTable(tables.SelectedValue.ToString());
+            foreach (List<string> data in res)
+            {
+                DataRow row = dt.NewRow();
+                for (int i = 0; i < columns.Count; i++)
+                {
+                    row[columns[i].Key] = data[i];
+
+                }
+                dt.Rows.Add(row);
+            }
+            DataView view = new DataView(dt);
+            dataGrid.ItemsSource = view;
+        }
+
+        private void ChangeButton_Click(object sender, RoutedEventArgs e)
+        {
+            object[] selectedElement = null;
+            string table = tables.SelectedValue.ToString();
+            if (dataGrid.SelectedValue != null)
+            {
+                selectedElement = (dataGrid.SelectedValue as DataRowView).Row.ItemArray;
+            }
+        }
+    }
+}
